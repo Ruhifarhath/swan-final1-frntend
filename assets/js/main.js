b79b5f40b83130f0d349d2ab68c96648f149a700
Ruhifarhath <ruhifarhath123@gmail.com>
2024-06-14 17:35:02 +0530

    responsiveness in pages

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,162 @@
-/**
-* Template Name: ZenBlog
-* Template URL: https://bootstrapmade.com/zenblog-bootstrap-blog-template/
-* Updated: Mar 17 2024 with Bootstrap v5.3.3
-* Author: BootstrapMade.com
-* License: https:///bootstrapmade.com/license/
-*/
+// /**
+// * Template Name: ZenBlog
+// * Template URL: https://bootstrapmade.com/zenblog-bootstrap-blog-template/
+// * Updated: Mar 17 2024 with Bootstrap v5.3.3
+// * Author: BootstrapMade.com
+// * License: https:///bootstrapmade.com/license/
+// */
+
+// document.addEventListener('DOMContentLoaded', () => {
+//   "use strict";
+
+//   /**
+//    * Sticky header on scroll
+//    */
+//   const selectHeader = document.querySelector('#header');
+//   if (selectHeader) {
+//     document.addEventListener('scroll', () => {
+//       window.scrollY > 100 ? selectHeader.classList.add('sticked') : selectHeader.classList.remove('sticked');
+//     });
+//   }
+
+//   /**
+//    * Mobile nav toggle
+//    */
+
+//   const mobileNavToogleButton = document.querySelector('.mobile-nav-toggle');
+
+//   if (mobileNavToogleButton) {
+//     mobileNavToogleButton.addEventListener('click', function(event) {
+//       event.preventDefault();
+//       mobileNavToogle();
+//     });
+//   }
+
+//   function mobileNavToogle() {
+//     document.querySelector('body').classList.toggle('mobile-nav-active');
+//     mobileNavToogleButton.classList.toggle('bi-list');
+//     mobileNavToogleButton.classList.toggle('bi-x');
+//   }
+
+//   /**
+//    * Hide mobile nav on same-page/hash links
+//    */
+//   document.querySelectorAll('#navbar a').forEach(navbarlink => {
+
+//     if (!navbarlink.hash) return;
+
+//     let section = document.querySelector(navbarlink.hash);
+//     if (!section) return;
+
+//     navbarlink.addEventListener('click', () => {
+//       if (document.querySelector('.mobile-nav-active')) {
+//         mobileNavToogle();
+//       }
+//     });
+//   });
+
+//   /**
+//    * Toggle mobile nav dropdowns
+//    */
+//   const navDropdowns = document.querySelectorAll('.navbar .dropdown > a');
+
+//   navDropdowns.forEach(el => {
+//     el.addEventListener('click', function(event) {
+//       if (document.querySelector('.mobile-nav-active')) {
+//         event.preventDefault();
+//         this.classList.toggle('active');
+//         this.nextElementSibling.classList.toggle('dropdown-active');
+
+//         let dropDownIndicator = this.querySelector('.dropdown-indicator');
+//         dropDownIndicator.classList.toggle('bi-chevron-up');
+//         dropDownIndicator.classList.toggle('bi-chevron-down');
+//       }
+//     })
+//   });
+
+//   /**
+//    * Scroll top button
+//    */
+//   const scrollTop = document.querySelector('.scroll-top');
+//   if (scrollTop) {
+//     const togglescrollTop = function() {
+//       window.scrollY > 100 ? scrollTop.classList.add('active') : scrollTop.classList.remove('active');
+//     }
+//     window.addEventListener('load', togglescrollTop);
+//     document.addEventListener('scroll', togglescrollTop);
+//     scrollTop.addEventListener('click', window.scrollTo({
+//       top: 0,
+//       behavior: 'smooth'
+//     }));
+//   }
+
+//   /**
+//    * Hero Slider
+//    */
+//   var swiper = new Swiper(".sliderFeaturedPosts", {
+//     spaceBetween: 0,
+//     speed: 500,
+//     centeredSlides: true,
+//     loop: true,
+//     slideToClickedSlide: true,
+//     autoplay: {
+//       delay: 3000,
+//       disableOnInteraction: false,
+//     },
+//     pagination: {
+//       el: ".swiper-pagination",
+//       clickable: true,
+//     },
+//     navigation: {
+//       nextEl: ".custom-swiper-button-next",
+//       prevEl: ".custom-swiper-button-prev",
+//     },
+//   });
+
+//   /**
+//    * Open and close the search form.
+//    */
+//   const searchOpen = document.querySelector('.js-search-open');
+//   const searchClose = document.querySelector('.js-search-close');
+//   const searchWrap = document.querySelector(".js-search-form-wrap");
+
+//   searchOpen.addEventListener("click", (e) => {
+//     e.preventDefault();
+//     searchWrap.classList.add("active");
+//   });
+
+//   searchClose.addEventListener("click", (e) => {
+//     e.preventDefault();
+//     searchWrap.classList.remove("active");
+//   });
+
+//   /**
+//    * Initiate glightbox
+//    */
+//   const glightbox = GLightbox({
+//     selector: '.glightbox'
+//   });
+
+//   /**
+//    * Animation on scroll function and init
+//    */
+//   function aos_init() {
+//     AOS.init({
+//       duration: 1000,
+//       easing: 'ease-in-out',
+//       once: true,
+//       mirror: false
+//     });
+//   }
+//   window.addEventListener('load', () => {
+//     aos_init();
+//   });
+
+// });
 
 document.addEventListener('DOMContentLoaded', () => {
   "use strict";
 
-  /**
-   * Sticky header on scroll
-   */
   const selectHeader = document.querySelector('#header');
   if (selectHeader) {
     document.addEventListener('scroll', () => {
@@ -19,12 +164,7 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   }
 
-  /**
-   * Mobile nav toggle
-   */
-
   const mobileNavToogleButton = document.querySelector('.mobile-nav-toggle');
-
   if (mobileNavToogleButton) {
     mobileNavToogleButton.addEventListener('click', function(event) {
       event.preventDefault();
@@ -38,11 +178,7 @@ document.addEventListener('DOMContentLoaded', () => {
     mobileNavToogleButton.classList.toggle('bi-x');
   }
 
-  /**
-   * Hide mobile nav on same-page/hash links
-   */
   document.querySelectorAll('#navbar a').forEach(navbarlink => {
-
     if (!navbarlink.hash) return;
 
     let section = document.querySelector(navbarlink.hash);
@@ -55,28 +191,28 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   });
 
-  /**
-   * Toggle mobile nav dropdowns
-   */
   const navDropdowns = document.querySelectorAll('.navbar .dropdown > a');
-
   navDropdowns.forEach(el => {
     el.addEventListener('click', function(event) {
       if (document.querySelector('.mobile-nav-active')) {
         event.preventDefault();
-        this.classList.toggle('active');
-        this.nextElementSibling.classList.toggle('dropdown-active');
-
+        console.log(this); // Debugging log
+        if (this.classList) {
+          this.classList.toggle('active');
+        }
+        if (this.nextElementSibling && this.nextElementSibling.classList) {
+          this.nextElementSibling.classList.toggle('dropdown-active');
+        }
         let dropDownIndicator = this.querySelector('.dropdown-indicator');
-        dropDownIndicator.classList.toggle('bi-chevron-up');
-        dropDownIndicator.classList.toggle('bi-chevron-down');
+        console.log(dropDownIndicator); // Debugging log
+        if (dropDownIndicator && dropDownIndicator.classList) {
+          dropDownIndicator.classList.toggle('bi-chevron-up');
+          dropDownIndicator.classList.toggle('bi-chevron-down');
+        }
       }
-    })
+    });
   });
 
-  /**
-   * Scroll top button
-   */
   const scrollTop = document.querySelector('.scroll-top');
   if (scrollTop) {
     const togglescrollTop = function() {
@@ -90,9 +226,6 @@ document.addEventListener('DOMContentLoaded', () => {
     }));
   }
 
-  /**
-   * Hero Slider
-   */
   var swiper = new Swiper(".sliderFeaturedPosts", {
     spaceBetween: 0,
     speed: 500,
@@ -113,33 +246,26 @@ document.addEventListener('DOMContentLoaded', () => {
     },
   });
 
-  /**
-   * Open and close the search form.
-   */
   const searchOpen = document.querySelector('.js-search-open');
   const searchClose = document.querySelector('.js-search-close');
   const searchWrap = document.querySelector(".js-search-form-wrap");
 
-  searchOpen.addEventListener("click", (e) => {
-    e.preventDefault();
-    searchWrap.classList.add("active");
-  });
+  if (searchOpen && searchClose && searchWrap) {
+    searchOpen.addEventListener("click", (e) => {
+      e.preventDefault();
+      searchWrap.classList.add("active");
+    });
 
-  searchClose.addEventListener("click", (e) => {
-    e.preventDefault();
-    searchWrap.classList.remove("active");
-  });
+    searchClose.addEventListener("click", (e) => {
+      e.preventDefault();
+      searchWrap.classList.remove("active");
+    });
+  }
 
-  /**
-   * Initiate glightbox
-   */
   const glightbox = GLightbox({
     selector: '.glightbox'
   });
 
-  /**
-   * Animation on scroll function and init
-   */
   function aos_init() {
     AOS.init({
       duration: 1000,
